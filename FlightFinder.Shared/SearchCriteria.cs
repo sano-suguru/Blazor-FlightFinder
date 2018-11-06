3d9fad51bf8c10bb2f0f7215d7bedd4efdbc500f
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-06 23:59:52 +0900

    [update]SearchCriteria.csを実装

--- FlightFinder.Shared/SearchCriteria.cs
+++ FlightFinder.Shared/SearchCriteria.cs
@@ -1,8 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FlightFinder.Shared {
-  class SearchCriteria {
+  public class SearchCriteria {
+    public SearchCriteria(
+      string fromAirport, string toAirport, TicketClass ticketClass
+    ) {
+      FromAirport = fromAirport;
+      ToAirport = toAirport;
+      TicketClass = ticketClass;
+      OutboundDate = DateTime.Now.Date;
+      ReturnDate = OutboundDate.AddDays(7);
+    }
+
+    public string FromAirport { get; }
+    public string ToAirport { get; }
+    public TicketClass TicketClass { get; }
+    public DateTime OutboundDate { get; }
+    public DateTime ReturnDate { get; }
   }
 }
