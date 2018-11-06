8c8a44b113a884a0f0321aa338afda029cb5e977
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-06 23:39:14 +0900

    [update]Itinerary.csを実装

--- FlightFinder.Shared/Itinerary.cs
+++ FlightFinder.Shared/Itinerary.cs
@@ -1,8 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FlightFinder.Shared {
   class Itinerary {
+    public Itinerary(
+      FlightSegment outBound, FlightSegment @return, decimal price
+    ) {
+      this.OutBound = outBound ?? throw new ArgumentNullException(nameof(outBound));
+      this.Return = @return ?? throw new ArgumentNullException(nameof(@return));
+      this.Price = price;
+    }
+
+    public FlightSegment OutBound { get; }
+    public FlightSegment Return { get; }
+    public decimal Price { get; }
+
+    public double TotalDurationhours =>
+      OutBound.DurationHours + Return.DurationHours;
+
+    public string AirlineName =>
+      (OutBound.AirLine == Return.AirLine) ? OutBound.AirLine : Return.AirLine;
   }
 }
