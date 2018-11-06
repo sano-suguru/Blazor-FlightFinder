666948caa82cfb8f782c8197c3820bdc78e4827f
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-07 00:08:36 +0900

    [udapte]AppState.csを実装

--- FlightFinder.Client/Services/AppState.cs
+++ FlightFinder.Client/Services/AppState.cs
@@ -1,5 +1,42 @@
-﻿namespace FlightFinder.Client.Services {
-  public class AppState {
+﻿using System;
+using Microsoft.AspNetCore.Blazor;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FlightFinder.Shared;
 
+namespace FlightFinder.Client.Services {
+  public class AppState {
+    public IReadOnlyList<Itinerary> SearchResults { get; private set; }
+    public bool SearchInProgress { get; private set; }
+
+    readonly List<Itinerary> shortList = new List<Itinerary>();
+    public IReadOnlyList<Itinerary> ShortList => shortList;
+
+    public event Action OnChange;
+
+    readonly HttpClient http;
+    public AppState(HttpClient httpClient) =>
+      this.http = httpClient;
+
+    public async Task Search(SearchCriteria criteria) {
+      SearchInProgress = true;
+      NotifyStateChanged();
+      SearchResults = await http.PostJsonAsync<Itinerary[]>("api/flightsearch", criteria);
+      SearchInProgress = false;
+      NotifyStateChanged();
+    }
+
+    public void AddtoShortList(Itinerary itinerary) {
+      shortList.Add(itinerary);
+      NotifyStateChanged();
+    }
+
+    public void RemoveFromShortList(Itinerary itinerary) {
+      shortList.Remove(itinerary);
+      NotifyStateChanged();
+    }
+
+    void NotifyStateChanged() => OnChange?.Invoke();
   }
 }
