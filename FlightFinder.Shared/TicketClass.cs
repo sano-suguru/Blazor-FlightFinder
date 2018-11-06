dabc4e550b02a61a24c1c2a00f7ecbfa43e2bb35
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-06 23:33:03 +0900

    [update]TicketClassを実装

--- FlightFinder.Shared/TicketClass.cs
+++ FlightFinder.Shared/TicketClass.cs
@@ -1,4 +1,15 @@
 ﻿namespace FlightFinder.Shared {
-  public class TicketClass {
+  public enum TicketClass : int {
+    Economy,
+    PremiumEconomy,
+    Business,
+    First
+  }
+  public static class TicketClassExtentions {
+    readonly static string[] ClassNames = {
+      "Economy", "Premium Economy", "Business", "First"
+    };
+    public static string ToDisplayString(this TicketClass ticketClass) =>
+      ClassNames[(int)ticketClass];
   }
 }
