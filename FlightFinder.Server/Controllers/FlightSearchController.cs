b18e381eedfcc9edaf0df6d3cd4b53f2a75a30f0
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-09 00:44:43 +0900

    [update]FlightSearchControllerを実装

--- FlightFinder.Server/Controllers/FlightSearchController.cs
+++ FlightFinder.Server/Controllers/FlightSearchController.cs
@@ -1,15 +1,51 @@
-﻿using System;
+﻿using FlightFinder.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
-namespace FlightFinder.Server.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FlightSearchController : ControllerBase
-    {
+namespace FlightFinder.Server.Controllers {
+  [Route("api/[controller]")]
+  public class FlightSearchController : Controller {
+    public async Task<IEnumerable<Itinerary>> Search([FromBody] SearchCriteria criteria) {
+      // web api 疑似遅延
+      await Task.Delay(500);
+      var random = new Random();
+      return Enumerable.Range(0, random.Next(1, 5))
+        .Select(_ => new Itinerary(
+          price: random.Next(100, 2000),
+          outBound: new FlightSegment(
+            airLine: RandomAirline(),
+            fromAirportCode: criteria.FromAirport,
+            toAirportCode: criteria.ToAirport,
+            departureTime: criteria.OutboundDate.AfterLessThanADay(),
+            returnTime: criteria.OutboundDate.AfterLessThanADay(),
+            durationHours: 2 + random.Next(10),
+            ticketClass: criteria.TicketClass
+          ),
+          @return: new FlightSegment(
+            airLine: RandomAirline(),
+            fromAirportCode: criteria.FromAirport,
+            toAirportCode: criteria.ToAirport,
+            departureTime: criteria.ReturnDate.AfterLessThanADay(),
+            returnTime: criteria.ReturnDate.AfterLessThanADay(),
+            durationHours: 2 + random.Next(10),
+            ticketClass: criteria.TicketClass
+          )
+        ));
     }
+
+    private string RandomAirline() =>
+      SampleData.Airlines[new Random().Next(SampleData.Airlines.Length)];
+  }
+
+  static class DateTimeExtentions {
+    public static DateTime AfterLessThanADay(this DateTime dateTime) {
+      var random = new Random();
+      return dateTime
+       .AddHours(random.Next(24))
+       .AddMinutes(5 * random.Next(12));
+    }
+  }
 }
