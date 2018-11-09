e1b4f6add1a45cb8d1aeaf81ebda77310c5a973d
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-09 23:41:26 +0900

    [update]AppStateをDI

--- FlightFinder.Client/Startup.cs
+++ FlightFinder.Client/Startup.cs
@@ -1,9 +1,11 @@
+using FlightFinder.Client.Services;
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FlightFinder.Client {
   public class Startup {
     public void ConfigureServices(IServiceCollection services) {
+      services.AddSingleton<AppState>();
     }
 
     public void Configure(IBlazorApplicationBuilder app) {
